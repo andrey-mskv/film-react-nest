bb17cce1bf6d2d6a27f59a8269e9355edcabe230
Andrei Moskvin <andrey.mskv@gmail.com>
2026-03-23 20:17:56 +0400

    feat/storage entities

---
+++ backend/src/films/entity/filmSchedule.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Film } from './films.entity';
+
+@Entity()
+export class Schedule {
+  @PrimaryColumn()
+  id: string;
+
+  @Column({ type: 'timestamptz' })
+  daytime: Date;
+
+  @Column()
+  hall: number;
+
+  @Column()
+  rows: number;
+
+  @Column()
+  seats: number;
+
+  @Column()
+  price: number;
+
+  @Column('text', { array: true })
+  taken: string[];
+
+  @ManyToOne(() => Film, (film) => film.schedule)
+  film: Film;
+}
