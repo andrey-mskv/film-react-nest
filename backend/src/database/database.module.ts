7ccccb2cd29f51a7a6388afb2e05a2b7ade5376a
Andrei Moskvin <andrey.mskv@gmail.com>
2026-03-23 20:10:11 +0400

    feat/database module

---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module, DynamicModule } from '@nestjs/common';
+import { AppConfigModule } from '../config/config.module';
+
+import configuration from '../config/configuration';
+
+import { PostgresDatabaseModule } from './postgres-database.module';
+import { MongoDatabaseModule } from './mongo-database.module';
+
+@Module({})
+export class DatabaseModule {
+  static register(): DynamicModule {
+    const config = configuration();
+    console.log('Кастомный конфиг: ', config);
+
+    const databaseModule =
+      config.database.driver === 'mongodb'
+        ? MongoDatabaseModule.register()
+        : PostgresDatabaseModule.register();
+
+    return {
+      module: DatabaseModule,
+      imports: [AppConfigModule, databaseModule],
+    };
+  }
+}
