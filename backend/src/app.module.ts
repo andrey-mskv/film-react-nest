fb80799cd3f60be3fd4b9963d95420a9cc770ab1
Andrei Moskvin <andrey.mskv@gmail.com>
2026-03-23 20:22:30 +0400

    rafactor/app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,30 +3,31 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import * as path from 'node:path';
 
-import { configProvider } from './app.config.provider';
+import configuration from './config/configuration';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { FilmsModule } from './films/films.module';
 import { OrderModule } from './order/order.module';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
+import { AppConfigModule } from './config/config.module';
+import { FilmImportService } from './import-films/import-films.service';
+import { Film } from './films/entity/films.entity';
+
+console.log(__dirname);
+console.log('cwd:', process.cwd());
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      cache: true,
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: path.join(__dirname, '..', 'public', 'content', 'afisha'),
-      serveRoot: '/content/afisha',
-    }),
-    MongooseModule.forRoot(
-      process.env.DATABASE_URL || 'mongodb://localhost:27017/afisha',
-      { connectionName: 'afisha' },
-    ),
+    AppConfigModule,
+    DatabaseModule.register(),
     FilmsModule,
     OrderModule,
+    ServeStaticModule.forRoot({
+      rootPath: path.join(process.cwd(), 'public', 'content', 'afisha'),
+      serveRoot: '/content/afisha',
+    }),
   ],
-  controllers: [],
-  providers: [configProvider],
 })
 export class AppModule {}
