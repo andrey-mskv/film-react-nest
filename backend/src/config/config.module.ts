3aa5e33a12e070c64aa976d7692a22e75f953c27
Andrei Moskvin <andrey.mskv@gmail.com>
2026-03-23 20:09:11 +0400

    feat/config module

---
+++ backend/src/config/config.module.ts
@@ -0,0 +1,21 @@
+// config.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration';
+import { validationSchema } from './validation';
+import { AppConfigService } from './config.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+      cache: true,
+      envFilePath: '.env',
+    }),
+  ],
+  providers: [AppConfigService],
+  exports: [AppConfigService],
+})
+export class AppConfigModule {}
